ee22dcb9a41d16925821422ca89cd2adf1d094d4
NikitaFatangare <nikitafatangare10@gmail.com>
2025-01-21 17:07:20 +0530

    Update A.java

--- git_demo/src/get_demo_/A.java
+++ git_demo/src/get_demo_/A.java
@@ -11,7 +11,10 @@ public class A {
 	{
 		System.out.println("m2-A");
 	}
-
+public  void m3()
+	{
+		System.out.println("m3-A");
+	}
 	public static void main(String[] args) {
 		System.out.println("git push");
 	}
