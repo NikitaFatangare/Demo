7ffee2dc70ca57299d451d2d2035cfdf47e1b3d1
NikitaFatangare <nikitafatangare10@gmail.com>
2025-01-21 17:00:44 +0530

    added m1 method

--- git_demo/src/get_demo_/A.java
+++ git_demo/src/get_demo_/A.java
@@ -1,6 +1,13 @@
 package get_demo_;
 
 public class A {
+	
+	public void m1()
+	{
+		
+		System.out.println("m1-A");
+	}
+	
 
 	public static void main(String[] args) {
 		System.out.println("git push");
